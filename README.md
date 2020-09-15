a6327bae4bc5551a6579617dd0e4c7c0b336fcc1
Esteban Borai <estebanborai@gmail.com>
2020-09-14 23:56:22 -0300

    feat: convert ascii to zw html and unicode

--- README.md
+++ README.md
@@ -1,7 +1,37 @@
 <div>
   <div align="center" style="display: block; text-align: center;">
-    <img src="https://via.placeholder.com/120" height="120" width="120" />
+    <img src="https://raw.githubusercontent.com/EstebanBorai/zwnbsp/main/assets/icon.png" height="120" width="120" />
   </div>
   <h1 align="center">zwnbsp</h1>
-  <h4 align="center">Zero with no-break space utils</h4>
+  <h4 align="center">Zero width character utilities</h4>
 </div>
+
+<div align="center">
+
+  [![Crates.io](https://img.shields.io/crates/v/zwnbsp.svg)](https://crates.io/crates/zwnbsp)
+  [![Documentation](https://docs.rs/zwnbsp/badge.svg)](https://docs.rs/zwnbsp)
+
+</div>
+
+## Installation
+
+```toml
+zwnbsp = "0.0.0"
+```
+
+## Release
+
+To release a new version you must tag with git and push to the `main` branch.
+
+```bash
+git tag -a v0.1.0 -m "First Release"
+git push origin main --follow-tags
+```
+
+## Contributing
+
+Every contribution to this project is welcome! Feel free to open a pull request or an issue.
+
+## License
+
+Licensed under MIT License.
--- lib/src/binary.rs
+++ lib/src/binary.rs
@@ -1,25 +1,72 @@
-pub fn to_binary(ascii: &str) -> String {
-  let mut binary = String::default();
-  let ascii: String = ascii.into();
+use crate::error::Error;
+use std::str::FromStr;
+use std::string::ToString;
 
-  for character in ascii.clone().into_bytes() {
-    binary += &format!("0{:b} ", character);
-  }
+/// Binary Representation of a `String` value
+pub struct Binary(String);
 
-  binary.pop();
+impl Binary {
+    /// Decodes a `Binary` back to the _ASCII_ representation
+    pub fn decode(&self) -> Result<String, Error> {
+        let as_u8: Vec<u8> = (0..self.0.len())
+            .step_by(9)
+            .map(|i| u8::from_str_radix(&self.0[i..i + 8], 2).expect("Unable to parse to u8"))
+            .collect();
 
-  binary
+        Ok(String::from_utf8(as_u8)?)
+    }
+}
+
+impl ToString for Binary {
+    fn to_string(&self) -> String {
+        self.0.clone()
+    }
+}
+
+impl FromStr for Binary {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut binary = String::default();
+        let ascii: String = s.into();
+
+        for character in ascii.into_bytes() {
+            binary += &format!("0{:b} ", character);
+        }
+
+        // removes the trailing space at the end
+        binary.pop();
+
+        Ok(Self(binary))
+    }
 }
 
 #[cfg(test)]
 mod test {
-  use super::*;
+    use super::*;
 
     #[test]
     fn converts_to_binary() {
-      let ascii = "Rustaceans";
-      let binary = "01010010 01110101 01110011 01110100 01100001 01100011 01100101 01100001 01101110 01110011";
+        let ascii = "Rustaceans";
+        let binary = "01010010 01110101 01110011 01110100 01100001 01100011 01100101 01100001 01101110 01110011";
 
-      assert_eq!(binary, to_binary(ascii));
+        assert_eq!(binary, Binary::from_str(ascii).unwrap().0);
+    }
+
+    #[test]
+    fn get_the_string_representation() {
+        let binary_raw = "01010010 01110101 01110011 01110100 01100001 01100011 01100101 01100001 01101110 01110011";
+        let binary_string = Binary::from_str("Rustaceans").unwrap();
+
+        assert_eq!(binary_raw, binary_string.to_string());
+    }
+
+    #[test]
+    fn converts_to_ascii() {
+        let original = "Rustaceans";
+        let decoded = Binary::from_str(original).unwrap();
+        let decoded = decoded.decode().unwrap();
+
+        assert_eq!(original.to_string(), decoded);
     }
 }
