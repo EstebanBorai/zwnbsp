be551abc91f8fff6231f8b46161f11fdeb308958
Esteban Borai <estebanborai@gmail.com>
2020-09-29 23:58:51 -0300

    refactor: move output to input

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -31,13 +31,13 @@
 //!
 mod binary;
 mod error;
-mod output;
+mod input;
 mod zero_width;
 mod zero_width_char;
 
 pub use binary::*;
 pub use error::*;
-pub use output::*;
+pub use input::*;
 pub use zero_width::*;
 pub use zero_width_char::*;
 
